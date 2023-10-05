a1b5cd775fc02c358be44b2b143885e7c97f7a53
harleydi <disneyharley26@gmail.com>
2023-10-05 13:15:21 -0400

    added mongoose and created mongoose connection to db

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected!!! Yea!!!'))
+  .catch((err) => console.log(err))
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,14 +20,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
